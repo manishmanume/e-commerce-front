2240b651164ad87d80f517ee87d4d75407604166
manishmanume <149936975+manishmanume@users.noreply.github.com>
2025-01-31 16:43:13 +0530

    change config

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,27 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-    plugins: [react()],
-    server: {
-        proxy: {
-            '/api': {
-                target: 'https://e-comerce-backend-lt0l.onrender.com',
-                changeOrigin: true,
-                secure: false,
-            },
-        },
+  plugins: [react()],
+  base: '/', 
+  build: {
+    outDir: 'dist', 
+  },
+  server: {
+    port: 3000,
+    proxy: {
+      '/api': {
+        target: 'https://e-comerce-backend-lt0l.onrender.com',
+        changeOrigin: true,
+        secure: false,
+      },
     },
+  },
+  resolve: {
+    alias: {
+      '@': '/src', 
+    },
+  },
+  esbuild: {
+    drop: ['console'], 
+  },
 });
